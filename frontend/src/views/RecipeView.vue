76074e42b85a48fa952cfa22ef7b92b8f42d8d5c
MissisOl <missisololol@gmail.com>
2025-09-07 00:38:29 +0300

    Добавлена страница рецепта

---
+++ frontend/src/views/RecipeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
+
+import { useRecipesStore } from '@/stores/recipes'
+
+const route = useRoute()
+
+const recipesStore =  useRecipesStore()
+const { recipe } = storeToRefs(recipesStore)
+const { getRecipeById } = recipesStore
+
+getRecipeById(route.params.id)
+</script>
+
+<template>
+  <div>id: {{ route.params.id }}</div>
+  <pre>{{ recipe }}</pre>
+</template>
+
+<style scoped></style>
