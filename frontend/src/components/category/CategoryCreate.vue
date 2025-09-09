1e6d94e266c992edd3cf2321ddb6d58fda87f0af
MissisOl <missisololol@gmail.com>
2025-09-09 15:52:25 +0300

    метод создания категории пренесен в store

--- frontend/src/components/category/CategoryCreate.vue
+++ frontend/src/components/category/CategoryCreate.vue
@@ -4,28 +4,19 @@ import { ref } from 'vue'
 import { useCategoriesStore } from '@/stores/categories'
 
 const categoriesStore = useCategoriesStore()
-const { getCategories } = categoriesStore
+const { getCategories, createCategory } = categoriesStore
 
 const params = ref({})
 
 async function saveCategory(e) {
   e.preventDefault()
-  try {
-    const res = await fetch('http://localhost:5002/categories', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(params.value)
-    })
-    const category_id = await res.json()
-    if (category_id) {
-      getCategories()
-    }
-    console.log('res', await res.json())
-    
-  } catch (error) {
-    console.log('error', error)
+
+  if (!params.value.name) {
+    return
+  }
+  const res = await createCategory(params.value)
+  if (res?.id) {
+    getCategories()
   }
 }
 </script>
--- frontend/src/stores/categories.js
+++ frontend/src/stores/categories.js
@@ -9,8 +9,24 @@ async function getCategories() {
   categories.value = await res.json()
 }
 
+async function createCategory(category) {
+  try {
+    const res = await fetch(`http://localhost:5002/categories`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(category)
+    })
+    return await res.json()
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
   return {
     categories,
     getCategories,
+    createCategory,
   }
 })
