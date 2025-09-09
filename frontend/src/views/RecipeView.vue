c7b4a5570a2dcc8f42f8d1da3be9d78dcd43bde9
MissisOl <missisololol@gmail.com>
2025-09-09 15:39:48 +0300

    добавлено удаление рецепта

--- frontend/src/views/RecipeView.vue
+++ frontend/src/views/RecipeView.vue
@@ -1,21 +1,26 @@
 <script setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
 
 import { useRecipesStore } from '@/stores/recipes'
 
+const router = useRouter()
 const route = useRoute()
 
 const recipesStore =  useRecipesStore()
 const { recipe } = storeToRefs(recipesStore)
-const { getRecipeById } = recipesStore
+const { getRecipeById, deleteRecipeById } = recipesStore
 
 getRecipeById(route.params.id)
+
+async function deleteRecipe(id) {
+  await deleteRecipeById(id)
+  router.push({ path: '/' })
+}
 </script>
 
 <template>
   <h1>{{ recipe.title }}</h1>
-    <img v-if="recipe.image" :src="recipe.image" alt="recipe image">
   <p>Ингредиенты: {{ recipe.ingredients }}</p>
   <p>Инструкции: {{ recipe.instructions }}</p>
   <p v-if="recipe.links">Ссылки: {{ recipe.links }}</p>
@@ -25,7 +30,7 @@ getRecipeById(route.params.id)
       <RouterLink :to="`/recipes/${recipe.id}/edit`">Редактировать</RouterLink>
     </button>
     <button type="button">Добавить комментарий</button>
-    <button type="button">Удалить</button>
+    <button type="button" @click="deleteRecipe(recipe.id)">Удалить</button>
   </div>
 </template>
 
