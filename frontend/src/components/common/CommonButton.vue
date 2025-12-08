3fd97c6a27ae56dbbdb18289145cb968539a71d8
MissisOl <missisololol@gmail.com>
2025-12-09 00:37:26 +0300

    Update CommonButton

--- frontend/src/components/common/CommonButton.vue
+++ frontend/src/components/common/CommonButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { buttonType, disabled } = defineProps({
+const { buttonType, disabled, className = ''  } = defineProps({
   buttonType: {
     type: String,
     default: 'button'
@@ -7,6 +7,10 @@ const { buttonType, disabled } = defineProps({
   disabled: {
     type: Boolean,
     default: false
+  },
+  className: {
+    type: String,
+    default: ''
   }
 })
 
@@ -19,7 +23,7 @@ const emit = defineEmits(['buttonAction'])
     :disabled="disabled"
     @click="emit('buttonAction')" 
     class="common-button"
-    :class="{'common-button--disabled': disabled}"
+    :class="className"
   >
     <slot name="icon"></slot>
     <slot name="text"></slot>
@@ -32,33 +36,35 @@ const emit = defineEmits(['buttonAction'])
   color: var(--text-color, var(--color-primary-foreground));
   border: 1px solid var(--cbtn-border, var(--color-primary));
   border-radius: calc(var(--radius) - 2px);
-  padding-block: .5rem;
-  padding-inline: 1rem;
   display: flex;
-  gap: .5rem;
   justify-content: center;
   align-items: center;
+  gap: .5rem;
   font-size: .875rem;
   line-height: 1.5rem;
   font-weight: 500;
   white-space: nowrap;
   cursor: pointer;
   transition: var(--transition-smooth);
+
+  padding-block: var(--btn-padding-block, 0.5rem);
+  padding-inline: var(--btn-padding-inline, 1rem);
 }
 
-.common-button--disabled {
-  background: hsl(from var(--cbtn-background) h s l / 0.1);
-  color: hsl(from var(--text-color) h s l / 0.3);
-  cursor: default;
-}
-
-@media (hover:hover) {
+@media (hover:hover) and (pointer: fine) {
   .common-button:not(:disabled):hover {
     --cbtn-fallback: var(--cbtn-background, var(--color-primary));
 
     background: var(--cbtn-hover, hsl(from var(--cbtn-fallback) h s l / 0.9));
     color: var(--text-hover, var(--color-primary-foreground));
   }
+}
+
+.common-button:disabled {
+  background: hsl(from var(--cbtn-background) h s l / 0.5);
+  color: hsl(from var(--text-color) h s l / 0.5);
+  border: 1px solid hsl(from var(--cbtn-border) h s l / 0.3);
+  cursor: default;
 }
 </style>
 
